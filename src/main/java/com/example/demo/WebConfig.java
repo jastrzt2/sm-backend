917a9245bf4fe3dfffe75503de05ff0679506d51
jastrzt2 <tjastrzebski102@gmail.com>
2024-04-30 18:58:26 +0200

    deploy poprawka w sing in 3

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,16 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Value("${frontend_url}")
+    private String frontendUrl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all paths
-                .allowedOrigins("http://localhost:5173") // Frontend server address
+                .allowedOrigins(frontendUrl) // Frontend server address
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Allowed HTTP methods
     }
 }
